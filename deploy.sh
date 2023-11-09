a4061419850f7f0703fbfd0fe36b4e533a0fd72f
Tuan Son Nguyen <ngtson216@gmail.com>
2023-11-10 00:32:03 +0700

    new-deploy-file

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r build/* root@178.128.28.105:/var/www/EU_CHOICE_ADMIN_APP/
+scp -r -P 19668 build/* root@103.175.147.216:/var/www/EUCHOICE_ADMIN_WEB/
 
 echo "Done!"
